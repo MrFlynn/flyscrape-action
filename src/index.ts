1762d1199ff9c55ac219560a309aac97a74460f0
Nick Pleatsikas <nick@pleatsikas.me>
2024-07-06 19:31:46 -0700

    Added logging messages to action.

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,18 @@ async function run(): Promise<void> {
     const inputs = config.getInputs();
     const platform = config.getPlatform();
 
+    core.info(`Running ${inputs.script} with flyscrape@${inputs.version}`);
+
     // Get asset URL from Github for the current platform.
     const assetURL = await github.getAssetURL(inputs.version, platform);
 
+    core.debug(`Downloading Flyscrape binary from ${assetURL}`);
+
     // Download, extract, and execute binary.
     const binaryPath = await binary.saveBinary(assetURL);
+    core.debug(
+      `Executing script: ${binaryPath} ${inputs.args.join(" ")} ${inputs.script}`,
+    );
     const output = await binary.executeScript(
       binaryPath,
       inputs.script,
